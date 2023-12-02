81489ae9044993406b06d9ae56907c4e9b20cb7a
Nguyen Chi Hieu <chihieunc99@gmail.com>
2023-12-02 22:00:28 +0700

    Add Hieu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import axios from 'axios';
 
-function App() {
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+import "./styles/styles.css";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState("all");
+
+  useEffect(() => {
+    // Fetch todos from local storage or an API if needed
+    // Example using local storage
+    const storedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    // Save todos to local storage
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (task) => {
+    setTodos([...todos, { id: Date.now(), task, completed: false }]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const clearTodos = () => {
+    setTodos([]);
+  };
+
+  const filterTodos = () => {
+    switch (filter) {
+      case "active":
+        return todos.filter((todo) => !todo.completed);
+      case "complete":
+        return todos.filter((todo) => todo.completed);
+      default:
+        return todos;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="all_in">
+        <h1># Todo App</h1>
+        
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="button_active">
+                  <button onClick={() => setFilter("all")}>All</button>
+                  <button onClick={() => setFilter("active")}>Active</button>
+                  <button onClick={() => setFilter("complete")}>
+                    Complete
+                  </button>
+                </div>
+                <hr />
+                <TodoForm addTodo={addTodo} />
+                <TodoList
+                  todos={filterTodos()}
+                  toggleTodo={toggleTodo}
+                  deleteTodo={deleteTodo}
+                />
+
+                {/* <div>
+                  <button onClick={clearTodos}>Clear All</button>
+                </div> */}
+              </>
+            }
+          />
+        </Routes>
+        
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
